ecee4d3a7cf2200ba57e52bfef0fa84f0f7d5648
victor <wktian1987@hotmail.com>
2026-04-28 13:59:05 +0800

    收到/***连接

--- index.js
+++ index.js
@@ -7,23 +7,25 @@ app.listen(process.env.PORT || 8080, () => {
 });
 
 app.post('/schedule', json(), async (req, res) => {
+    console.log("收到/schedule连接");
     const { HandleUnreadGmails } = await import("./handleUnreadGmails.js");
     await HandleUnreadGmails(req, res);
 }
 );
 
 app.post('/tgBot', json(), async (req, res) => {
+    console.log("收到/tgBot连接");
     const { HandleTgBot } = await import("./handleTgBot.js");
     await HandleTgBot(req, res);
 }
 );
 
 app.post('/tradingview', json(), async (req, res) => {
+    console.log("收到/tradingview连接");
     const { HandleTV } = await import("./handleTV.js");
     await HandleTV(req, res);
 }
 );
-
 
 app.use((err, req, res, next) => {
     console.error('全局捕获错误:', err.stack);
