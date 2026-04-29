9827dc365615874196014392a6fd9f6e9b369ef0
victor <wktian1987@hotmail.com>
2026-04-29 17:15:23 +0800

    regular update

--- handleTV.js
+++ handleTV.js
@@ -30,20 +30,85 @@ export async function HandleTV(newDatasFromTV) {
         newDatasFromTV.tvUpdateTime   = GetTimeStringWithOffset(8, newDatasFromTV.timestamp);
         newDatasFromTV.gcpGetTime     = GetTimeStringWithOffset(8);
 
-        if (datas.realTradeTime < newDatasFromTV.timestamp) {
+        if (newDatasFromTV.timestamp > datas.realTradeTime) {
+            if (datas.crtFund === "toFill") {
+                // 数据初始化
+                datas.crtFund       =  Number(datas.inFund);
+                datas.crtCoin       =  Number(datas.inCoin);
+                datas.allFund       =  datas.crtFund + datas.crtCoin * newDatasFromTV.BaseCoinPrice;
+                datas.initialFund   =  datas.allFund;
+                datas.hghestFund    =  datas.allFund;
+                datas.lowestFund    =  datas.allFund;
+                datas.allCoin       =  datas.crtFund / newDatasFromTV.BaseCoinPrice + datas.crtCoin;
+                datas.initialCoin   =  datas.allCoin;
+                datas.hghestCoin    =  datas.allCoin;
+                datas.lowestCoin    =  datas.allCoin;
+                datas.allPosition   =  0;
+                datas.usedMargin    =  0;
+                datas.freeMargin    =  datas.crtFund + datas.crtCoin * newDatasFromTV.BaseCoinPrice * Number(datas.BaseCoinHairCut);
+                datas.allTradeFee   =  0;
+                datas.allFundFee    =  0;
+                datas.netProfit     =  0;
+                datas.openProfit    =  0;
+                datas.avgBuyPrice   =  0;
+            }
+
+            // 收到新消息数据初始化
+            datas.netProfit         =  Number(datas.netProfit);
+            datas.avgBuyPrice       =  Number(datas.avgBuyPrice);
+            datas.openProfit        =  Number(datas.allPosition) * (newDatasFromTV.TradingSymbolPrice - datas.avgBuyPrice) ;
+            datas.crtFund           =  Number(datas.crtFund) + datas.openProfit;
+            datas.crtCoin           =  Number(datas.crtCoin);
+            datas.allFund           =  datas.crtFund + datas.crtCoin * newDatasFromTV.BaseCoinPrice;
+            datas.initialFund       =  Number(datas.initialFund);
+            datas.hghestFund        =  datas.allFund > Number(datas.hghestFund) ? datas.allFund : Number(datas.hghestFund);
+            datas.lowestFund        =  datas.allFund < Number(datas.lowestFund) ? datas.allFund : Number(datas.lowestFund);
+            datas.allCoin           =  datas.crtFund / newDatasFromTV.BaseCoinPrice + datas.crtCoin;
+            datas.initialCoin       =  Number(datas.initialCoin);
+            datas.hghestCoin        =  datas.allCoin > Number(datas.hghestCoin) ? datas.allCoin : Number(datas.hghestCoin);
+            datas.lowestCoin        =  datas.allCoin < Number(datas.lowestCoin) ? datas.allCoin : Number(datas.lowestCoin);
+            datas.allPosition       =  Number(datas.allPosition);
+            datas.usedMargin        =  datas.allPosition * newDatasFromTV.TradingSymbolPrice / Number(datas.leverage);
+            datas.freeMargin        =  datas.crtFund + datas.crtCoin * newDatasFromTV.BaseCoinPrice * Number(datas.BaseCoinHairCut) - datas.usedMargin;
+            datas.allTradeFee       =  Number(datas.allTradeFee);
+            datas.allFundFee        =  Number(datas.allFundFee);
+            datas.crt_initialFund   =  (datas.allFund - datas.initialFund) / datas.initialFund  ;
+            datas.crt_hghestFund    =  (datas.allFund - datas.hghestFund ) / datas.hghestFund   ;
+            datas.crt_lowestFund    =  (datas.allFund - datas.lowestFund ) / datas.lowestFund   ;
+            datas.crt_initialCoin   =  (datas.allCoin - datas.initialCoin) / datas.initialCoin  ;
+            datas.crt_hghestCoin    =  (datas.allCoin - datas.hghestCoin ) / datas.hghestCoin   ;
+            datas.crt_lowestCoin    =  (datas.allCoin - datas.lowestCoin ) / datas.lowestCoin   ;
+
+
+        } else {
+            // 未到交易时刻的逻辑
+            console.log("收到TradingView消息, 但未到交易时刻");
         }
 
-
-
-
-
-
-
-
-
-
-
-
+        newDatasFromTV.netProfit        =  datas.netProfit       
+        newDatasFromTV.avgBuyPrice      =  datas.avgBuyPrice     
+        newDatasFromTV.openProfit       =  datas.openProfit      
+        newDatasFromTV.crtFund          =  datas.crtFund         
+        newDatasFromTV.crtCoin          =  datas.crtCoin         
+        newDatasFromTV.allFund          =  datas.allFund         
+        newDatasFromTV.initialFund      =  datas.initialFund     
+        newDatasFromTV.hghestFund       =  datas.hghestFund      
+        newDatasFromTV.lowestFund       =  datas.lowestFund      
+        newDatasFromTV.allCoin          =  datas.allCoin         
+        newDatasFromTV.initialCoin      =  datas.initialCoin     
+        newDatasFromTV.hghestCoin       =  datas.hghestCoin      
+        newDatasFromTV.lowestCoin       =  datas.lowestCoin      
+        newDatasFromTV.allPosition      =  datas.allPosition     
+        newDatasFromTV.usedMargin       =  datas.usedMargin      
+        newDatasFromTV.freeMargin       =  datas.freeMargin      
+        newDatasFromTV.allTradeFee      =  datas.allTradeFee     
+        newDatasFromTV.allFundFee       =  datas.allFundFee      
+        newDatasFromTV.crt_initialFund  =  datas.crt_initialFund 
+        newDatasFromTV.crt_hghestFund   =  datas.crt_hghestFund  
+        newDatasFromTV.crt_lowestFund   =  datas.crt_lowestFund  
+        newDatasFromTV.crt_initialCoin  =  datas.crt_initialCoin 
+        newDatasFromTV.crt_hghestCoin   =  datas.crt_hghestCoin  
+        newDatasFromTV.crt_lowestCoin   =  datas.crt_lowestCoin  
 
 
         const writeToRange = newDatasFromTV.sheetTitle + '!A:B'; // 指定操作 A 到 B 列
@@ -64,11 +129,13 @@ export async function HandleTV(newDatasFromTV) {
         });
 
         let newDatasFromSheet   =  { timestamp: 0 };
-        let attampts            =  0 
+        let attampts            =  0;
+        let waitTime            =  1000;
         while (attampts < 60 && Number(newDatasFromSheet.timestamp) < newDatasFromTV.timestamp) {
-            await new Promise(res => setTimeout(res, 5000));
+            await new Promise(res => setTimeout(res, waitTime));
             newDatasFromSheet = Object.fromEntries(await GetDataFromSheet(sheets, spreadsheetId, ranges.toGCP));
             attampts    += 1;
+            waitTime    =  attampts * 1000;
         }
         if (Number(newDatasFromSheet.timestamp) >= newDatasFromTV.timestamp) {
             console.log('✔ TV数据写入表格成功');
