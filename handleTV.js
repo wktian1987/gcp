cab6b0c1ac53444606992113891d741464a44e70
victor <wktian1987@hotmail.com>
2026-05-05 11:21:55 +0800

    regular update

--- handleTV.js
+++ handleTV.js
@@ -108,8 +108,16 @@ async function SendOrderToBroker(S, sheets, spreadsheetId) {
     
     const res_broker =  Object.fromEntries(await GetDataFromSheet(sheets, spreadsheetId, 'simBroker!A2:B29') )  ;
 
-    S.ing_orderID   =  res_broker.orderID  ;
-    
+    S.ing_orderID		    = res_broker.orderID        ;
+    S.ing_confirmDate		= res_broker.confirmDate    ;
+    S.ing_confirmPrice		= res_broker.confirmPrice   ;
+    S.ing_getProfit		    = res_broker.getProfit      ;
+    S.ing_avgBuyPrice		= res_broker.avgBuyPrice    ;
+    S.ing_tradeFee		    = res_broker.tradeFee       ;
+    S.ing_allFund		    = res_broker.allFund        ;
+    S.ing_allCoin		    = res_broker.allCoin        ;
+    S.ing_orderStatus		= res_broker.orderStatus    ;
+
     return S ;
 }
 
