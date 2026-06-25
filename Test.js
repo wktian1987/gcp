f587f2f16c54932d717842ad1249e08ca58f44ec
victor <wktian1987@hotmail.com>
2026-06-25 17:40:51 +0800

    debug

--- Test.js
+++ Test.js
@@ -1,4 +1,4 @@
-import { GetGS, GetSpreadsheetID, SendTG, Sleep } from "./utility.js"
+import { GetGS, GetSpreadsheetID, SendTG, Sleep, UpdateGS } from "./utility.js"
 
 export async function test(chat_id) {
     await SendTG('TEST信号处理开始', '这里是test()正在处理...', chat_id) ;
@@ -18,9 +18,25 @@ async function testFunctionFromGS0() {
     }
 }
 
+// 在下面函数中写入测试逻辑, 并将函数复制到A1单元格中, 然后去TG执行test指令
+// 这样就可以在不重启cloudrun的情况下, 快速测试或者部署新的函数了
 async function functionA1(chat_id) {
     
+    // 测试大范围内下范围的内容修改
+    const bigRegion = 'test!B3:F22';
+    const newContent = [
+        [2, 2, 2] ,
+        [2, 2, 2] ,
+        [2, 2, 2] ,
+        [2, 2, 2] ,
+        [2, 2, 2] ,
+        [2, 2, 2] ,
+        [2, 2, 2] ,
+        [2, 2, 2] 
+    ] ;
 
+    const spreadsheetID     = await GetSpreadsheetID('TradingBot_00') ;
+    await UpdateGS(spreadsheetID, bigRegion, newContent) ;
 
 
 
