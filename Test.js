e6754799b75c8f24d3776166d0bbc648fcc00c77
victor <wktian1987@hotmail.com>
2026-06-25 15:22:19 +0800

    update

--- Test.js
+++ Test.js
@@ -1,7 +1,7 @@
 import { SendTG, Sleep } from "./utility.js"
 
 export async function test(chat_id) {
-    SendTG('TEST信号处理开始', '这里是test()正在处理...', chat_id).catch(() => { });
+    await SendTG('TEST信号处理开始', '这里是test()正在处理...', chat_id) ;
 
-    SendTG('TEST信号处理结束', 'TEST信号处理结束', chat_id).catch(() => { });
+    await SendTG('TEST信号处理结束', 'TEST信号处理结束', chat_id) ;
 }
