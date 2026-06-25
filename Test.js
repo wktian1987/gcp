eb31db180a347cf435506d6076290fc21e1efd3c
victor <wktian1987@hotmail.com>
2026-06-25 17:57:29 +0800

    debug

--- Test.js
+++ Test.js
@@ -1,4 +1,4 @@
-import { GetGS, GetSpreadsheetID, SendTG, Sleep, UpdateGS } from "./utility.js"
+import { GetGS, GetSpreadsheetID, SendTG, Sleep, UpdateGS, ClearGS } from "./utility.js"
 
 export async function test(chat_id) {
     await SendTG('TEST信号处理开始', '开始加载00文件中的A1函数...', chat_id) ;
@@ -20,16 +20,6 @@ export async function test(chat_id) {
     await SendTG('TEST信号处理结束', 'TEST信号处理结束', chat_id) ;
 }
 
-async function testFunctionFromGS0(chat_id) {
-    const functionRegion    = 'test!A1' ;
-    const spreadsheetID     = await GetSpreadsheetID('TradingBot_00') ;
-    const functionString    = await GetGS(spreadsheetID, functionRegion) ;
-    eval(functionString) ;
-    try { await functionA1(spreadsheetID, chat_id) } catch (e) {
-        throw new Error('A1函数执行失败: ' + e.message)
-    }
-}
-
 // 在下面函数中写入测试逻辑, 并将函数复制到A1单元格中, 然后去TG执行test指令
 // 这样就可以在不重启cloudrun的情况下, 快速测试或者部署新的函数了
 async function functionA1(spreadsheetID, chat_id) {
@@ -47,6 +37,7 @@ async function functionA1(spreadsheetID, chat_id) {
         [2, 2, 2] 
     ] ;
 
+    await ClearGS(spreadsheetID, bigRegion, newContent) ;
     await UpdateGS(spreadsheetID, bigRegion, newContent) ;
 
 
