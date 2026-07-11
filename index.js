59dc44f6059565ff054aabfbddf76785c4900c04
victor <wktian1987@hotmail.com>
2026-07-11 08:46:37 +0800

    debug

--- index.js
+++ index.js
@@ -124,8 +124,8 @@ async function HandleSignalList() {
         if (SignalList.length > 0 && runningTasks < MaxRunningTasks) {
             taskNumber += 1;
             runningTasks += 1 ;
-            console.log(`... ... 开始处理第${taskNumber}个任务，共有${runningTasks}个任务同时运行，尚有${SignalList.length}个信号等待处理`)
-            const toHandleSignal = SignalList.pop()
+            const toHandleSignal = SignalList.pop() ;
+            console.log(`... ... 开始处理第${taskNumber}个任务，共有${runningTasks}个任务同时运行，任务队列中尚有${SignalList.length}个信号等待处理`)
             HandleSignal(toHandleSignal.url, toHandleSignal.body).catch(() => { }).finally(()=>{runningTasks -= 1});
         
             await Sleep(1000) ;
